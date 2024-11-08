a164b5b7dfc43509dea640c657b67f31455dffcd
Juan Geyer <juanjogeyer@gmail.com>
2024-11-08 13:14:46 -0300

    Update repository

--- app/repository.py
+++ app/repository.py
@@ -1,21 +1,9 @@
-from typing import List
 from app import db
-from app.model import Stock
+from app.models import Stock
 
 class StockRepository:
-
-    def all(self) -> List[Stock]:
-        return db.session.query(Stock).all()
     
     def add(self, stock: Stock) -> Stock:
         db.session.add(stock)
         db.session.commit()
         return stock
-    
-    def delete(self, stock: Stock) -> None:
-        db.session.delete(stock)
-        db.session.commit()
-        return None
-    
-    def find(self, id: int) -> Stock:
-        return db.session.query(Stock).filter(Stock.id == id).one_or_none()
