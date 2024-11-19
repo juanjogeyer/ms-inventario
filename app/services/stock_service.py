8ee8437cb5eb91ae7d4760ed3522e45cbf44f023
Juan Geyer <juanjogeyer@gmail.com>
2024-11-19 19:35:53 -0300

    Add cache

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -1,6 +1,7 @@
 from app.repository import StockRepository
 from app.models import Stock
 from datetime import datetime
+from app import cache
 
 repository = StockRepository()
 
@@ -12,6 +13,7 @@ class StockService:
             stock.fecha_transaccion = stock.fecha_transaccion if stock.fecha_transaccion is not None else datetime.now()
             stock.entrada_salida = 2 # Salida de Producto
             result = repository.add(stock)
+            cache.set(f'stock_{stock.id}', result, timeout=60)
         return result
 
     def ingresar(self, stock: Stock) -> Stock:
@@ -20,4 +22,5 @@ class StockService:
             stock.fecha_transaccion = stock.fecha_transaccion if stock.fecha_transaccion is not None else datetime.now()
             stock.entrada_salida = 1 # Entrada de Producto
             result = repository.add(stock)
+            cache.set(f'stock_{stock.id}', result, timeout=60)
         return result
